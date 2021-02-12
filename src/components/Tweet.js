2daffbd9b3d0b8e9aaf4a5e879f43b340839b9ac
tatiana-passarella <tatiana.passarella@gmail.com>
2021-02-12 15:05:00 +0100

    console.log of all tweets informations

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,11 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
     render() {
+        const { tweet } = this.props
+
+        if (tweet === null) {
+          return <p>This tweet doesn't exist</p>
+        }
         console.log(this.props)
         return (
             <div className='tweet'>
@@ -16,10 +21,13 @@ class Tweet extends Component {
 
 function mapStateToProps ({authedUser, users, tweets}, { id }) {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
